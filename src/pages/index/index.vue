0076bea20c1de901cb13bf2c83703ecbab82fba6
noahlam <284516433@qq.com>
2018-09-29 21:06:34 +0800

    chore: rename

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="warp">
+  <div class="wrap">
    我是首页
+    <div class="tab">我是 tab 内容</div>
   </div>
 </template>
 
@@ -18,4 +19,17 @@
   }
 </script>
 <style scoped>
+  .wrap {
+    width: 100%;
+    height: calc(100vh - 100rpx);
+    background: red;
+  }
+  .tab{
+    width: 100%;
+    height: 100rpx;
+    background: green;
+    position: fixed;
+    left: 0;
+    bottom:0;
+  }
 </style>
