33edec32544c67903a16130b3e9ebcf0a8251182
noahlam <284516433@qq.com>
2018-10-07 11:15:22 +0800

    chore: category page

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ export default {
   methods: {
   },
   created () {
-    // wx.cloud.init()
   },
   onLaunch () {
     wx.cloud.init({
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App'
-// import post from './utils/post'
 
 Vue.config.productionTip = false
-// Vue.prototype.$post = post.post
 App.mpType = 'app'
 
 const app = new Vue(App)
